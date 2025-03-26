06802653b96ff96633f574c2cec92244e10985eb
fanidevweb@gmail.com <fanidevweb@gmail.com>
2025-03-25 23:19:15 -0300

    configMongp-configMogoose

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express, { json } from 'express';
 import { router } from './routes';
+import 'dotenv/config';
+import { setupMongo } from './database';
 
-const app = express();
+setupMongo().then(() => {
+	const app = express();
 
-app.use(json());
-app.use(router);
+	app.use(json());
+	app.use(router);
 
-app.listen(3333, () => console.log('App is running at port 3333!'));
+	app.listen(3334, () => console.log('App is running at port 3334!'));
+});
